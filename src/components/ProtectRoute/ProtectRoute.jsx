c015582c89cc7f23521edfb6ee742f4f531a3144
Ivan Sukhovarov <isukhovarov8@gmail.com>
2023-02-27 00:27:49 +0300

    ProtectRoute-fix

--- src/components/ProtectRoute/ProtectRoute.jsx
+++ src/components/ProtectRoute/ProtectRoute.jsx
@@ -1,19 +1,20 @@
 import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
-
+import { getCookie } from '../../utils/cookies';
 
 export default function ProtectRoute({ element, anonymous = false }) {
 
   const location = useLocation();
   const from = location.state?.from || '/';
   const userInfo = useSelector((state) => state.userRequestReducer.userInfo);
+  const accessToken = getCookie('accessToken');
 
   if (anonymous && userInfo) {
     return <Navigate to={ from } />;
   }
 
   if (!anonymous && !userInfo) {
-    return <Navigate to="/login" state={{ from: location}}/>;
+    return accessToken || userInfo ? element : <Navigate to="/login" state={{ from: location}}/>;
   }
 
   return element;
